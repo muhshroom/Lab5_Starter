8959ff2e4ecb5e1ea85a0c25ac351fba1167764a
Diana Kostina <dkostina@ucsd.edu>
2026-05-05 20:41:06 -0700

    Explore implementation + README answers

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Lab 5 - Starter
 Name: Diana Kostina
 GitHub Pages URL: [Expose Party Horn](https://muhshroom.github.io/Lab5_Starter/)
+
+## Pt 3. Check Your Understanding Answers
+1. No, a "message" feature of a messaging application is a large moving application, not a small scale part. Unit testing specifically check one small, often isolated component of the whole workflow, instead of complex features like messaging. 
+2. Yes, a "max message length" is just one part of a larger moving application which can be tested with simple unit tests.
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,47 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const voiceSelect = document.getElementById("voice-select");
+  const button = document.querySelector("button");
+
+  populateVoices();
+  speechSynthesis.onvoiceschanged = populateVoices;
+  button.addEventListener("click", speakText);
+}
+let voices = [];
+
+function populateVoices() {
+  const voiceSelect = document.getElementById("voice-select");
+  voices = speechSynthesis.getVoices();
+  voiceSelect.innerHTML = `
+    <option value="select" disabled selected>Select Voice:</option>
+  `;
+  voices.forEach((voice, index) => {
+    const option = document.createElement("option");
+    option.value = index;
+    option.textContent = `${voice.name} (${voice.lang})`;
+    voiceSelect.appendChild(option);
+  });
+}
+
+function speakText() {
+  const text = document.getElementById("text-to-speak").value;
+  const voiceSelect = document.getElementById("voice-select");
+  const faceImage = document.querySelector("#explore img");
+  if (!text.trim()) return;
+  const utterance = new SpeechSynthesisUtterance(text);
+  const selectedVoice = voices[voiceSelect.value];
+  if (selectedVoice) {
+    utterance.voice = selectedVoice;
+  }
+
+  utterance.onstart = () => {
+    faceImage.src = "assets/images/smiling-open.png";
+  };
+
+  utterance.onend = () => {
+    faceImage.src = "assets/images/smiling.png";
+  };
+
+  speechSynthesis.speak(utterance);
 }
